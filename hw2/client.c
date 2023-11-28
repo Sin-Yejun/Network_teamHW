994fa5fe2cbf649fd598d710a4374161f0e25339
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-11-28 22:31:19 +0900

    Buffer change

--- hw2/client.c
+++ hw2/client.c
@@ -5,16 +5,19 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/select.h>
-#define BUFSIZE 30
-#define NICK_SIZE 30
+#define BUFSIZE 4096
+#define NICK_SIZE 50
 
-void error_handling(const char *message) {
+void error_handling(const char *message)
+{
     perror(message);
     exit(1);
 }
 
-int main(int argc, char **argv) {
-    if (argc != 4) {
+int main(int argc, char **argv)
+{
+    if (argc != 4)
+    {
         printf("Usage: %s <ip> <port> <nickname>\n", argv[0]);
         exit(1);
     }
@@ -39,43 +42,44 @@ int main(int argc, char **argv) {
 
     fd_set reads, temps;
     int fd_max;
-	
+
     FD_ZERO(&reads);
     FD_SET(0, &reads); // 0 is file descriptor of stdin
     FD_SET(sock, &reads);
     fd_max = sock;
-    
-    while (1) {
-	temps = reads;
-    	if (select(fd_max+1, &temps, 0, 0, NULL) == -1)
-        	error_handling("select() error");
-	if (FD_ISSET(sock, &temps)) {
-        	// Receive message from server
-        	memset(message, 0, sizeof(message));
-		int str_len = read(sock, message, sizeof(message));
-       		if (str_len == -1)
-            		error_handling("read() error");
-		message[str_len] = '\0';
-        	printf("%s", message);
 
-    	}	
-	if (FD_ISSET(0, &temps)) {
-        	// Send message to server
-	        memset(message, 0, sizeof(message));
-	      	fgets(message, sizeof(message), stdin);
-	        int len = strlen(message);
-	        if(message[len-1] == '\n')
-	            message[len-1]='\0';
-	        if (strcmp(message, "Q\0") == 0){
-	            break;
-	        }
- 	       write(sock, message, strlen(message)+1);
-  	}	
-
+    while (1)
+    {
+        temps = reads;
+        if (select(fd_max + 1, &temps, 0, 0, NULL) == -1)
+            error_handling("select() error");
+        if (FD_ISSET(sock, &temps))
+        {
+            // Receive message from server
+            memset(message, 0, sizeof(message));
+            int str_len = read(sock, message, sizeof(message));
+            if (str_len == -1)
+                error_handling("read() error");
+            message[str_len] = '\0';
+            printf("%s", message);
+        }
+        if (FD_ISSET(0, &temps))
+        {
+            // Send message to server
+            memset(message, 0, sizeof(message));
+            fgets(message, sizeof(message), stdin);
+            int len = strlen(message);
+            if (message[len - 1] == '\n')
+                message[len - 1] = '\0';
+            if (strcmp(message, "Q\0") == 0)
+            {
+                break;
+            }
+            write(sock, message, strlen(message) + 1);
+        }
     }
 
     close(sock);
 
-   return 0;
+    return 0;
 }
-
